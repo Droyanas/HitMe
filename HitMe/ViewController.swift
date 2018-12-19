0e81f5ee98f3a536eae2723b02d9228d03f3be9f
Константин Анисимов <aleksandr@macbook-pro-aleksandr-2.local>
2018-12-19 11:44:48 +0300

    Initial Commit

---
+++ HitMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HitMe
+//
+//  Created by Александр on 12/19/18.
+//  Copyright © 2018 Александр. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
